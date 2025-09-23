295974dec0dd6bf27e8c231a3c7fa79bc8c612e2
Swayam-creator <swayamallewar7@gmail.com>
2025-09-23 20:26:21 +0530

    handling errors

--- src/index.js
+++ src/index.js
@@ -3,9 +3,24 @@ const app=express();
 
 const PORT=8080;
 
-app.use('/',(req,res)=>{
-    res.send("Hi from root route")
+app.get('/a', (req, res,next) => {
+    try {
+ console.log("log from /a+ route");
+ throw new Error("New error from here");
+  res.send("Hi from /a+ route");
+    } catch (error) {
+        next(error);
+    }
+},
+);
+app.use((err,req,res,next)=>{
+    if(err){
+        console.log("finding status from here"+err.stack);
+        res.status(err.status||500).send({message:"You encountered an error"});
+    }
 })
+
+
 
 app.listen(PORT,()=>{
     console.log(`Server started on ${PORT}`);
