c56b2194542b922e43e27d1788319c5e06d529da
Swayam-creator <swayamallewar7@gmail.com>
2025-10-06 23:52:07 +0530

    connection logic to implement

--- src/routers/connection.router.js
+++ src/routers/connection.router.js
@@ -2,7 +2,5 @@ import express from 'express';
 
 const router=express.Router();
 
-router.post('/send/interested/:userId',connectionRequestController);
-router.post('/send/ignored/:userId',connectionRequestController);
-router.post('/review/accept/:requestId',connectionRequestController);
-router.post('/review/reject/:requestId',connectionRequestController);
+router.post('/send/:status/:userId',connectionRequestController);
+router.post('/review/:reviewstatus/:requestId',connectionRequestController);
